9766821a02b9154c182b54526d651ca1200f65c7
gorkem <113816295+gorkem1940@users.noreply.github.com>
2022-10-02 00:35:43 +0300

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,7 @@
+const chalk = require("chalk");
+const moment = require("moment");
+const Discord = require("discord.js");
+const ayarlar = require('../ayarlar.json');
+module.exports = async client => {
+  client.user.setPresence({ activity: { type: "LISTENING", name: `Pars ❤️ Tanrısıs `}, status: 'dnd' })
+};
